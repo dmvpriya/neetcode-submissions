b55921c1e110e448cbacd9f083c45650eeda3a80
Mohanaa Vishnu Priya Durla <71810443+dmvpriya@users.noreply.github.com>
2026-07-09 07:22:08 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // Hashtable<Character, Integer> s1 = new Hashtable<>();
+        // Hashtable<Character, Integer> s2 = new Hashtable<>();
+        // for(char ch:s.toCharArray()){
+        //     if(s1.containsKey(ch)){
+        //         s1.put(ch, s1.get(ch)+1);
+        //     } else{
+        //         s1.put(ch,1);
+        //     }
+        // }
+        // for(char ch:t.toCharArray()){
+        //     if(s2.containsKey(ch)){
+        //         s2.put(ch, s2.get(ch)+1);
+        //     } else{
+        //         s2.put(ch,1);
+        //     }
+        // }
+        // if(s1.equals(s2)){
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+
+        // To reduce the space complexity
+        Hashtable<Character, Integer> s1 = new Hashtable<>();
+        for(char ch:s.toCharArray()){
+            s1.put(ch, s1.getOrDefault(ch, 0)+1);
+        }
+        Hashtable<Character, Integer> s2 = new Hashtable<>();
+        for(char ch:t.toCharArray()){
+            s2.put(ch, s2.getOrDefault(ch, 0)+1);
+        }
+        if(s1.equals(s2)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}

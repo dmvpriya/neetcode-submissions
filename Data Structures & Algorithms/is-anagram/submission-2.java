cd18107303bf1458965a944e578ef8a0d3424165
Mohanaa Vishnu Priya Durla <71810443+dmvpriya@users.noreply.github.com>
2026-07-09 07:17:08 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Hashtable<Character, Integer> s1 = new Hashtable<>();
+        Hashtable<Character, Integer> s2 = new Hashtable<>();
+        for(char ch:s.toCharArray()){
+            if(s1.containsKey(ch)){
+                s1.put(ch, s1.get(ch)+1);
+            } else{
+                s1.put(ch,1);
+            }
+        }
+        for(char ch:t.toCharArray()){
+            if(s2.containsKey(ch)){
+                s2.put(ch, s2.get(ch)+1);
+            } else{
+                s2.put(ch,1);
+            }
+        }
+        if(s1.equals(s2)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
